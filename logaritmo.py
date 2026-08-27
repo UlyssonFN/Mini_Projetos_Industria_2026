0cf31edb5e50df1172c1100a2aa6fe2430a355f1
Ulysson Fontenele <ulysson52@gmail.com>
2026-08-27 20:08:11 -0300

    Add Script de cálculo de logaritmo- ainda incompleto

---
+++ logaritmo.py
@@ -0,0 +1,45 @@
+#Logaritmo
+#Volume de um Cubo V = a³
+#Paralelepipedo Retângulo V = a*b*c
+#Cilindro 'Sb = área da base do cilindro' então V = Sb*h, lembrando que para saber a área da base A = Pi*Raio²
+#Cálculo de Log| Loga N = x <-> a^x = N
+#Logaritmo decimnal: É o log de base 10, normalmente o logaritmo decimal não tem sua base especificada. Se escreve log =a já entende que a base é 10
+#Logaritmo natural: Também chamado de logaritmo neperiano. Sua base é o número e=2,71828...(número de Euler) Loge a = Ln a
+import math
+
+print("Aula de Logaritmos - Escolha uma opção abaixo")
+print("1 - Volume de um Cubo")
+print("2 - Volume de um Paralelepipedo Retângulo")
+print("3 - Volume de um Cilindro")
+resp = int(input("Responda: ")) 
+match(resp):
+    case 1:
+        print("Descubra o volume de um Cubo")
+        area = float(input("Digite a área do cubo"))
+        volumeC = area**3
+        #cm para m
+        cmpm = area / 10 / 10
+        #m³
+        m3 = cmpm**3
+        print("O volume do seu Cubo é: ",volumeC,"cm3")
+        print("Convertendo para m³: ",m3,"m³")
+    case 2:
+        print("Descubra o volume de um Paralelepipedo Retângulo")
+        area = float(input("Digite a: "))
+        base = float(input("Digite b: "))
+        profundidade = float(input("Digite c: "))
+        volumeP = area*base*profundidade
+        litros = (area*10)*(base*10)*(profundidade*10)
+        print("O volume do seu Paralelepipedo é: ",volumeP,"m³")
+        print("Convertendo para litros: ",litros,"L")
+    case 3:
+        print("Descubra o volume de um Cilindro")
+        pi = math.pi
+        raioN = float(input("Digite o raio da área do cilindro: "))
+        raio = raioN**2
+        area_circulo = pi*raio
+        #Sb=area_circulo
+        altura = float(input("Digite a altura do cilindro(h): "))
+        volumeC = area_circulo*altura
+        print("A área do seu circulo é: ",area_circulo)
+        print("O volume do seu Cilindro é: ",volumeC)
