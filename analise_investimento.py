4aa50aae10bf3bc6b7541c51deff3b5296b4bd17
Ulysson Fontenele <ulysson52@gmail.com>
2026-09-01 16:52:44 -0300

    Adcionado busca de ação internacional

--- analise_investimento.py
+++ analise_investimento.py
@@ -1,17 +1,54 @@
 #Verificar bitcoin tempo real
 import pandas as pd
 import yfinance as yf
+from notifypy import Notify
+from funcoes import space, repeticao
 
-acao_entrada = input("Digite a ação que procura: ")
+a = 1
+while a==1:
+    print("Escolha a ação:")
+    print("1 - Ação BR (interna)")
+    print("2 - Ação Externa")
+    resposta = int(input("Digite uma opção: "))
+    match(resposta):
+        case 1:
+            space()
+            acao_entrada = input("Digite a ação que procura: ")
+            sufixobr = '.SA'
 
-acao = yf.Ticker(acao_entrada)
+            acao = yf.Ticker(acao_entrada+sufixobr)
 
-print("---Informe o período--- ")
-print("1d = 1 dia, 1mo = 1 mês, 1y = 1 ano, ytd = inicio do ano até hoje, max = todo histórico")
-periodo = input("Digite o período: ")
+            print("---Informe o período--- ")
+            print("1d = 1 dia, 1mo = 1 mês, 1y = 1 ano, ytd = inicio do ano até hoje, max = todo histórico")
+            periodo = input("Digite o período: ")
 
-if periodo == '1d' or periodo == '1mo' or periodo == '1y' or periodo == 'ytd' or periodo =='max':
-    df = ticker.history()
-else:
-    print("valor inválido")
+            if periodo == '1d' or periodo == '1mo' or periodo == '1y' or periodo == 'ytd' or periodo =='max':
+                df = acao.history(periodo)
+                print("------Hoje (Ação BR)------",acao)
+                print(round(df,2))
+                print("------Detalhado (Ação BR)------", acao)
+                print(round(df.describe(),2))
+                a = repeticao()
+            else:
+                print("valor inválido")
+                a = repeticao()
+        case 2:
+            space()
+            acao_entrada = input("Digite a ação que procura: ")
+            
+            acao = yf.Ticker(acao_entrada)
 
+            print("---Informe o período--- ")
+            print("1d = 1 dia, 1mo = 1 mês, 1y = 1 ano, ytd = inicio do ano até hoje, max = todo histórico")
+            periodo = input("Digite o período: ")
+
+            if periodo == '1d' or periodo == '1mo' or periodo == '1y' or periodo == 'ytd' or periodo =='max':
+                df = acao.history(periodo)
+                print("------Hoje (Ação Ex)------",acao)
+                print(round(df,2))
+                print("------Detalhado (Ação Ex)------", acao)
+                print(round(df.describe(),2))
+                a = repeticao()
+            else:
+                print("valor inválido")
+                a = repeticao()
