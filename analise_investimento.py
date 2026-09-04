2642262661017f1fbdce8353a32660f481643b1a
Ulysson Fontenele <ulysson52@gmail.com>
2026-09-04 17:37:08 -0300

    adicionado comparativo de investido_antes vs investido_agora, condicional de lucro ou prejuizo

--- analise_investimento.py
+++ analise_investimento.py
@@ -5,13 +5,10 @@ from notifypy import Notify
 from funcoes import space, repeticao
 import datetime as dt
 
-
-
-
-
 a = 1
 while a==1:
-    hoje = dt.datetime.today()
+    hoje = dt.date.today()
+    print(hoje)
     hora = dt.time()
     print("Escolha a ação:")
     print("1 - Ação BR (interna)")
@@ -28,7 +25,6 @@ while a==1:
             print("---Informe o período--- ")
             print("1d = 1 dia, 1mo = 1 mês, 1y = 1 ano, ytd = inicio do ano até hoje, max = todo histórico")
             periodo = input("Digite o período: ")
-            investimento = float(input("Digite o valor do investimento: "))
             cotas = int(input("Digite a quantidade de cotas compradas: "))
             data_compra = input("Digite a data da compra: aaaa-mm-dd ")
             #converter para dataframer
@@ -40,7 +36,7 @@ while a==1:
             #["Close"] → quero a coluna Close
             #.iloc[0] → quero o primeiro valor dessa coluna
             precofechamento = filtro['Close'].iloc[0]
-            investimento_inicial = investimento * cotas * precofechamento
+            investimento_inicial = cotas * precofechamento
             space()
             print('O seu investimento inicial foi de :',round(investimento_inicial,2))
             space()           
@@ -48,12 +44,28 @@ while a==1:
             if periodo == '1d' or periodo == '1mo' or periodo == '1y' or periodo == 'ytd' or periodo =='max':
                 df = acao.history(periodo)
                 df2 = df.filter(items=['Open','Close'])
-                investimento_hoje = 0
                 print("------Hoje (Ação BR)------",acao)
                 print(round(df2,2))
                 print("------Detalhado (Ação BR)------", acao)
                 print(round(df2.describe(),2))
-                a = repeticao()
+                
+                #calculo de investimento atual
+                precofechamento_hoje = acao.history('1d')
+                filtro_hoje = precofechamento_hoje[precofechamento_hoje.index.date == hoje]
+                precofechamento_hojef = filtro_hoje['Close'].iloc[0]
+                investimento_atual = precofechamento_hojef * cotas
+                space()
+                print('O seu investimento Hoje é de :',round(investimento_atual,2))
+                space()    
+                
+                lucro_prejuizo = investimento_atual - investimento_inicial
+                
+                if lucro_prejuizo < 0:
+                    print("Seu prejuízo é de :", lucro_prejuizo)
+                    a = repeticao()
+                else:
+                    print("Seu Lucro é de :", lucro_prejuizo)
+                    a = repeticao()
             else:
                 print("valor inválido")
                 a = repeticao()
